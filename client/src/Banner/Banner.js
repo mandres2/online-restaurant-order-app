e5aa7c16e89344d193ba8a245cccf481c71cd7c2
Micah Andres <mandres1@uw.edu>
2019-12-03 19:58:10 -0800

    uploaded files that contain both Authentication and Menu - menu does not work yet

---
+++ client/src/Banner/Banner.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Banner = styled.div`
+   height: 30px;
+   width: 100%;
+   background-position: center;
+   background-size: cover;
+   filter: contrast(75%);
+    `
